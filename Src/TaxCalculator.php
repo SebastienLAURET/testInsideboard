7cb48614a1026dbe4d7ec3cc5b6f89d078e7aa9d
lauret_s <lauret_s@epitech.eu>
2018-05-06 21:18:09 +0200

    add header file

---
+++ Src/TaxCalculator.php
@@ -0,0 +1,11 @@
+<?php
+## CONTROLLERS
+require("Src/Controllers/TaxCalculatorController.php");
+
+## MODELS
+require ("Src/Models/Bill.php");
+require ("Src/Models/Line.php");
+
+## CONSTANTS
+require ("Src/Constants/LinesTypesCont.php")
+ ?>
